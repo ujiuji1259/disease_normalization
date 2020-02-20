938cf2da3f3b331150d36202fb7f9899db5d2f45
氏家翔吾 <ujiieshougo@mm-dhcp-125-173.naist.jp>
2020-02-20 16:09:31 +0900

    minor change

---
+++ calculate_accuracy.py
@@ -0,0 +1,21 @@
+with open('result/result.txt', 'r') as f:
+    lines = [line for line in f.read().split('\n') if line != '']
+
+pos = 0
+neg = 0
+positive_example = []
+negative_example = []
+for line in lines:
+    word, true, pred = line.split('\t')
+    if true == pred:
+        pos += 1
+        positive_example.append(line)
+    else:
+        neg += 1
+        negative_example.append(line)
+
+print(pos/(pos+neg))
+with open('result/true_example.txt', 'w') as f:
+    f.write('\n'.join(positive_example))
+with open('result/false_example.txt', 'w') as f:
+    f.write('\n'.join(negative_example))
